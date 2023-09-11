d7b713b18eb7be4a55370d40e1a7991f3674fbf7
Neelesh1907 <119998532+Neelesh1907@users.noreply.github.com>
2023-09-11 18:20:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 function decimalToBinary(num) {
   //Write you code here
-  
+  let n = num, ans="";
+	while(n!==0){
+		rem = n%2;
+		n = n/2;
+		ans = rem+ans;
+	}
+	return ans;
 }
 
 window.decimalToBinary = decimalToBinary;
